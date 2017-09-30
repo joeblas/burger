7185e0b3a3e660e46e3e3871aca3a3a7661a5a15
Joe Gallegos <joebgallegos@gmail.com>
2017-09-29 19:48:52 -0600

    Addressed error with connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var config = require("../config.json");
+// var config = require("../config.json");
 var connection;
 
 if (process.env.JAWSDB_URL) {
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     host: "localhost",
     user: "root",
-    password: config.SQL_Password,
+    password: null,
     database: "burgers_db"
   });
 }
